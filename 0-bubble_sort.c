c6d7ad934656e395aded5622201e2f7231cdd628
walid <jabiriwalid555@gmail.com>
2023-07-25 20:22:37 +0100

    Sorting algorithms & Big O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+/**
+ * swap - swap two integers
+ *
+ * @ptr1: first ptr
+ * @ptr2: second ptr
+ */
+void swap(int *ptr1, int *ptr2 ) 
+{
+	int temp;
+	temp = *ptr1;
+	*ptr1 = *ptr2;
+	*ptr2 = temp;
+}
+
+/**
+ * bubble_sort - sort integers
+ *
+ * @array: the array to be stored
+ * @size: the size of the arr
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] >  array[j + 1])
+			{
+				swap(array + j, array + j + 1);
+				print_array(array, size);
+			}
+		}
+	}
+}
